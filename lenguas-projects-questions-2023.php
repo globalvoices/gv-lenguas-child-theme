1f474b144f74e7f67218218ce64d674d920de4b3
Jer Clarke <jer@simianuprising.com>
2023-07-25 17:30:01 -0600

    ⚙️ directorio 2023: update metadata field questions to latest versions, splitting them into their own metaboxes in the process.

--- lenguas-projects-questions-2023.php
+++ lenguas-projects-questions-2023.php
@@ -63,56 +63,88 @@ function gv_microgrants_lenguas_2023_custom_metadata_manager_admin_init() {
 	/**
 	 * Register a group for pages and posts
 	 */
-	x_add_metadata_group('gv_custom_metadata_posts', array('post'), array(
-		'label' => 'GV Custom Metadata',
+	x_add_metadata_group('lenguas_project_questions_2023', array('post'), array(
+		'label' => 'Lenguas Project Questions 2023',
 		'priority' => 'high',
 	));
 
 	/**
-	 * Project Contact Name
+	 * Project Contact Name - Backend only
 	 */
 	x_add_metadata_field( 'project-contact-name', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
-		'label' => 'Tu nombre',
+		'group' => 'lenguas_project_questions_2023',
+		'label' => 'Nombre del contacto',
 		'field_type' => 'text',
 	));
 	/**
-	 * Project Contact Email
+	 * Project Contact Email - Backend only
 	 */
 	x_add_metadata_field( 'project-contact-email', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
-		'label' => 'Tu correo electrónico: (e-mail)',
+		'group' => 'lenguas_project_questions_2023',
+		'label' => 'Tu correo electrónico (e-mail)',
 		'field_type' => 'textarea',
 	));
 	/**
-	 * Project Language
+	 * Project Language that wasn't in list - Backend only
 	 */
 	x_add_metadata_field( 'project-language-other', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
+		'group' => 'lenguas_project_questions_2023',
 		'label' => 'Si tu lengua no figura en las opciones disponibles, escríbalo aquí.',
 		'field_type' => 'textarea',
 	));
 	/**
-	 * Project year
+	 * Project year - Backend only
 	 */
 	x_add_metadata_field( 'project-year', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
+		'group' => 'lenguas_project_questions_2023',
 		'label' => '¿En qué año comenzó tu proyecto?',
 		'field_type' => 'textarea',
 	));
 	/**
-	 * Project city
+	 * Project city - Backend only
 	 */
 	x_add_metadata_field( 'project-city', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
+		'group' => 'lenguas_project_questions_2023',
 		'label' => '¿Desde qué ciudad se implementa su proyecto?',
 		'field_type' => 'textarea',
 	));
 	/**
+	 * Project country that wasn't in list - Backend only
+	 */
+	x_add_metadata_field( 'project-country-other', array('post'), array(
+		'group' => 'lenguas_project_questions_2023',
+		'label' => 'Si tu país no figura en las opciones, anótalo aquí.',
+		'field_type' => 'textarea',
+	));
+	/**
+	 * Project online presence - Backend only
+	 */
+	x_add_metadata_field( 'project-online-presence', array('post'), array(
+		'group' => 'lenguas_project_questions_2023',
+		'label' => 'Introduce la dirección de tus cuentas de redes sociales y/o sitio web (eg Instagram, TikTok, Twitter, Facebook, YouTube, etc. http://www.instagram.com/cuenta_instagram).',
+		'field_type' => 'textarea',
+	));
+	/**
+	 * Project public contact - Backend only
+	 */
+	x_add_metadata_field( 'project-public-contact', array('post'), array(
+		'group' => 'lenguas_project_questions_2023',
+		'label' => 'Si alguien de otro proyecto tiene preguntas, ¿qué opción prefieren para ser contactados?',
+		'field_type' => 'textarea',
+	));
+
+	// ! Fields below apply to the old 2020 and older forms, but are kept so the data can be accessed
+
+	x_add_metadata_group('lenguas_project_questions_2020', array('post'), array(
+		'label' => 'OLD/LEGACY Lenguas Project Questions 2020',
+		'priority' => 'high',
+	));
+
+	/**
 	 * Project type
 	 */
 	x_add_metadata_field( 'project-type', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
+		'group' => 'lenguas_project_questions_2020',
 		'label' => '¿Cómo se identifica tu proyecto/iniciativa?',
 		'field_type' => 'textarea',
 	));
@@ -120,7 +152,7 @@ function gv_microgrants_lenguas_2023_custom_metadata_manager_admin_init() {
 	 * Project challenges
 	 */
 	x_add_metadata_field( 'project-challenges', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
+		'group' => 'lenguas_project_questions_2020',
 		'label' => '¿Cuáles son algunos de los desafíos que enfrenta? (lingüísticos, técnicos, socio-culturales, gestión)',
 		'field_type' => 'textarea',
 	));
@@ -128,7 +160,7 @@ function gv_microgrants_lenguas_2023_custom_metadata_manager_admin_init() {
 	 * Project Strategies
 	 */
 	x_add_metadata_field( 'project-strategies', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
+		'group' => 'lenguas_project_questions_2020',
 		'label' => '¿Qué estrategias está empleando para abordar los desafíos identificados?',
 		'field_type' => 'textarea',
 	));
@@ -136,7 +168,7 @@ function gv_microgrants_lenguas_2023_custom_metadata_manager_admin_init() {
 	 * Project Audience
 	 */
 	x_add_metadata_field( 'project-audience', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
+		'group' => 'lenguas_project_questions_2020',
 		'label' => '¿Para quién está pensado el proyecto? ¿Tienen un tipo de población especial en mente? ¿Quién es su público objetivo?',
 		'field_type' => 'textarea',
 	));
@@ -144,7 +176,7 @@ function gv_microgrants_lenguas_2023_custom_metadata_manager_admin_init() {
 	 * Project Assistance
 	 */
 	x_add_metadata_field( 'project-assistance', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
+		'group' => 'lenguas_project_questions_2020',
 		'label' => '¿Cuáles crees que serían las herramientas de capacitación y planeación más importantes que requiere tu proyecto para continuar, para consolidarse y para conseguir sus objetivos?',
 		'field_type' => 'textarea',
 	));
@@ -153,7 +185,7 @@ function gv_microgrants_lenguas_2023_custom_metadata_manager_admin_init() {
 	 * Project website
 	 */
 	x_add_metadata_field( 'project-website', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
+		'group' => 'lenguas_project_questions_2020',
 		'label' => 'Sitio web',
 		'field_type' => 'text',
 	));
@@ -161,7 +193,7 @@ function gv_microgrants_lenguas_2023_custom_metadata_manager_admin_init() {
 	 * Project Facebook
 	 */
 	x_add_metadata_field( 'project-facebook', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
+		'group' => 'lenguas_project_questions_2020',
 		'label' => 'Facebook',
 		'field_type' => 'text',
 	));
@@ -169,7 +201,7 @@ function gv_microgrants_lenguas_2023_custom_metadata_manager_admin_init() {
 	 * Project Twitter
 	 */
 	x_add_metadata_field( 'proposal-twitter', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
+		'group' => 'lenguas_project_questions_2020',
 		'label' => 'Twitter',
 		'field_type' => 'text',
 	));
@@ -177,7 +209,7 @@ function gv_microgrants_lenguas_2023_custom_metadata_manager_admin_init() {
 	 * Project Instagram
 	 */
 	x_add_metadata_field( 'project-instagram', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
+		'group' => 'lenguas_project_questions_2020',
 		'label' => 'Instagram',
 		'field_type' => 'text',
 	));
@@ -185,7 +217,7 @@ function gv_microgrants_lenguas_2023_custom_metadata_manager_admin_init() {
 	 * Project YouTube
 	 */
 	x_add_metadata_field( 'project-youtube', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
+		'group' => 'lenguas_project_questions_2020',
 		'label' => 'YouTube',
 		'field_type' => 'text',
 	));
@@ -193,7 +225,7 @@ function gv_microgrants_lenguas_2023_custom_metadata_manager_admin_init() {
 	 * Project other platforms
 	 */
 	x_add_metadata_field( 'project-otherplatforms', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
+		'group' => 'lenguas_project_questions_2020',
 		'label' => 'Otras plataformas',
 		'field_type' => 'textarea',
 	));
@@ -201,7 +233,7 @@ function gv_microgrants_lenguas_2023_custom_metadata_manager_admin_init() {
 	 * Project team
 	 */
 	x_add_metadata_field( 'project-team', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
+		'group' => 'lenguas_project_questions_2020',
 		'label' => '(Opcional) Comparta los nombres de las personas clave (y sus roles) en el, o el nombre de la organización al frente del, proyectos',
 		'field_type' => 'textarea',
 	));
@@ -209,7 +241,7 @@ function gv_microgrants_lenguas_2023_custom_metadata_manager_admin_init() {
 	 * Project team contact
 	 */
 	x_add_metadata_field( 'project-team-contact', array('post'), array(
-		'group' => 'gv_custom_metadata_posts',
+		'group' => 'lenguas_project_questions_2020',
 		'label' => '¿Cuál es la mejor manera para que alguien les contacte si tienen preguntas?',
 		'field_type' => 'textarea',
 	));
@@ -220,7 +252,7 @@ function gv_microgrants_lenguas_2023_custom_metadata_manager_admin_init() {
 	 */	
 	define('GV_LENGUAS_PROJECTS_METADATA_DEFINED', true);
 }
-add_action( 'current_screen', 'gv_microgrants_lenguas_2030_custom_metadata_manager_admin_init');
+add_action( 'current_screen', 'gv_microgrants_lenguas_2023_custom_metadata_manager_admin_init');
 
 /**
  * Register postmeta inserts for display on the frontend if they have the `directorio` category
